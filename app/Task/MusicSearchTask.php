8bf83da959e0fe1e040b2c356c50802a322ffd1c
isesame <isesame_live@163.com>
2020-09-08 18:48:09 +0800

    点歌后歌单更新

--- app/Task/MusicSearchTask.php
+++ app/Task/MusicSearchTask.php
@@ -5,7 +5,9 @@ namespace App\Task;
 
 use App\Services\MusicService;
 use Hyperf\Di\Annotation\Inject;
+use Hyperf\Redis\Redis;
 use Hyperf\Task\Annotation\Task;
+use Hyperf\Utils\ApplicationContext;
 use Hyperf\WebSocketServer\Sender;
 
 class MusicSearchTask
@@ -30,10 +32,17 @@ class MusicSearchTask
         $searchResult = $this->musicService->searchMusic($data);
 
         if ($searchResult['type'] == 'success') {
-            $this->sender->push((int)$data['id'], json_encode([
-                "type" => "list",
-                "data" => $searchResult['list']
-            ]));
+            //成功更新所有人歌单
+            $redis = ApplicationContext::getContainer()->get(Redis::class);
+            $connections = $redis->hKeys(MusicService::USER_SEND_KEY);
+            if(count($connections) > 0) {
+                foreach($connections as $id) {
+                    $this->sender->push((int)$id, json_encode([
+                        "type" => "list",
+                        "data" => $searchResult['list']
+                    ]));
+                }
+            }
         } else {
             $this->sender->push((int)$data['id'], json_encode([
                 "type" => "msg",
